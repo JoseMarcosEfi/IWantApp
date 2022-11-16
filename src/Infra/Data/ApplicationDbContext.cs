1744201e16c903b04233d65f6fdaef0151153e5e
Jose marcos <72453138+SakakibaraKun@users.noreply.github.com>
2022-11-16 15:15:53 -0300

    Add class product, entity, category and dbcontext

---
+++ src/Infra/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿namespace IWantApp.Infra.Data;
+
+using IWantApp.Domain.Products;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        
+    }
+
+
+}
